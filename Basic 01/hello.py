fb2b4e25b37c71369be3a105ba6c14452847eff6
Zenith-hasan <abuhasan323382@gmail.com>
2025-04-22 15:32:03 +0600

    first step :)

---
+++ Basic 01/hello.py
@@ -0,0 +1,22 @@
+# vector
+
+[1,2,3,4,5]  # Yes, this is array ,list in the python but it's alos called vector.
+[[2,3,4,5][3,4,5]] # This is called Matrix
+
+# Matrix can also be written like the following :
+[
+	[4,5,6]
+	[4,5,6]
+]
+
+# What you have seen earlied is a 2D (Two Dimentional) Matrix, but martix can be n-dimentionla as well. It's also called n-dimentional data 
+
+# Syntex for the n-dimentional matrices
+
+[
+	[4,5,6]
+	[6,7,8]
+	[7,8,9]
+	[2,3,4]
+	[1,2,3]
+] # This is an example of the 5-dimentional matrices or 5-dimentional data
